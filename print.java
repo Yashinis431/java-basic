5d24d5dabfa53c902976a9439d94e744fc9902b4
Yashini S <yashinis431@gmail.com>
2025-01-07 22:45:05 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,11 @@
+class prograa{
+    public static void main(String[] args){
+        String m = ("movie name");
+        String a = ("Actor name");
+        float r =5.5f;
+        String b = "100 cr";
+        boolean hit = true;
+        System.out.println(m+""+a+""+r+""+b+""+hit);
+
+    }
+}
